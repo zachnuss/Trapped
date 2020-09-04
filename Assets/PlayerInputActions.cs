42fd179f540ec221e80b680c4027d756d3863e34
Dylan Loe <dylanloe@gmail.com>
2020-09-03 18:57:44 -0700

    Player Input Plugin
    Along with my test scene and incomplete player movement

---
+++ Assets/PlayerInputActions.cs
@@ -0,0 +1,227 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInputActions"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerControls"",
+            ""id"": ""24488270-164a-433e-a1e7-f70694c34257"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""e4718020-6da6-4994-b84c-10934fd26d91"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""bec6aeb9-4bfb-4375-9559-842825f366a8"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""31cce612-4bbf-4db2-a1e1-6495c5a396fb"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Joystick"",
+                    ""id"": ""f28c03b9-4c15-4fa7-8117-2d4eeb355907"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""13abad0b-f823-44a5-910d-93a2cb587b5f"",
+                    ""path"": ""<Joystick>/stick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""58d56c65-5282-48c1-a7ce-fcb5582f7a3b"",
+                    ""path"": ""<Joystick>/stick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""af68c6cb-8425-4b12-9e43-20a35594e5e7"",
+                    ""path"": ""<Joystick>/stick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8bdf15ad-52ed-41c5-9673-1ca62f2cb6a5"",
+                    ""path"": ""<Joystick>/stick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1436550c-2bcc-48f5-b9a1-dcb7e002a22d"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""820dd8e6-4d0a-4ec8-8b27-928113c17f5a"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerControls
+        m_PlayerControls = asset.FindActionMap("PlayerControls", throwIfNotFound: true);
+        m_PlayerControls_Move = m_PlayerControls.FindAction("Move", throwIfNotFound: true);
+        m_PlayerControls_Interact = m_PlayerControls.FindAction("Interact", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerControls
+    private readonly InputActionMap m_PlayerControls;
+    private IPlayerControlsActions m_PlayerControlsActionsCallbackInterface;
+    private readonly InputAction m_PlayerControls_Move;
+    private readonly InputAction m_PlayerControls_Interact;
+    public struct PlayerControlsActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public PlayerControlsActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerControls_Move;
+        public InputAction @Interact => m_Wrapper.m_PlayerControls_Interact;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerControlsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerControlsActions instance)
+        {
+            if (m_Wrapper.m_PlayerControlsActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMove;
+                @Interact.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnInteract;
+                @Interact.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnInteract;
+                @Interact.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnInteract;
+            }
+            m_Wrapper.m_PlayerControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Interact.started += instance.OnInteract;
+                @Interact.performed += instance.OnInteract;
+                @Interact.canceled += instance.OnInteract;
+            }
+        }
+    }
+    public PlayerControlsActions @PlayerControls => new PlayerControlsActions(this);
+    public interface IPlayerControlsActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnInteract(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+
+/// <summary>
+/// Dylan Loe
+/// 
+/// Basic movement system
+/// 
+/// Notes:
+/// - 6 has 6 faces, each face will therefore incorperate different downward pull on player
+/// - each side therefore has different axis for movement
+/// - will keep track of current face with enum
+/// 
+/// - regardless which way player is faceing, w is forward, s is back, a is left and d is right
+/// </summary>
+
+public enum faceStatus
+{
+    none,
+    face1,
+    face2,
+    face3,
+    face4,
+    face5,
+    face6
+}
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerInputActions controls;
+
+
+    //tells current face player is resting on
+    public faceStatus playerFaceStatus;
+    //current player position
+    public Vector3 playerPos;
+    //current player velocity
+    public Vector3 playerVelocity;
+
+    public float movementSpeed = 1.0f;
+    Vector2 movementInput;
+    //rotation
+    float _turnSpeed = 20f;
+    float _angle;
+    Quaternion targetRotation;
+
+    //Camera
+    public Camera playerCam;
+
+    //awake
+    private void Awake()
+    {
+        controls = new PlayerInputActions();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerFaceStatus = faceStatus.face1;
+    }
+
+    // Used for physics 
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    // used for updating values and variables
+    private void Update()
+    {
+        //GetPlayerInput();
+    }
+
+    //players moves with WASD
+    void GetPlayerInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += (-transform.forward) * movementSpeed * Time.deltaTime;
+
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += (-transform.right) * movementSpeed * Time.deltaTime;
+
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * movementSpeed * Time.deltaTime;
+
+        }
+
+        switch (playerFaceStatus)
+        {
+             //top face
+            case faceStatus.face1:
+                //gravity is down the y axis
+                break;
+            case faceStatus.face2:
+                //gravity is up the z axis
+                break;
+            case faceStatus.face3:
+                // gravity is down the z axis (reverse from face2)
+                break;
+            case faceStatus.face4:
+                //gravity is up the x axis 
+                break;
+            case faceStatus.face5:
+                //gravity is down the x axis (reverse from face4)
+                break;
+                //bottom face
+            case faceStatus.face6:
+                //gravity is up the y axis (reverse from face1)
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// NOTE: NEEDS PACKAGE
+    /// </summary>
+    /// <returns></returns>
+
+
+
+    //movement
+
+    private void OnMove(InputValue value)
+    {
+        movementInput = value.Get<Vector2>();
+    }
+
+    //joysticks
+    public float JoystickH()
+    {
+        float r = 0.0f;
+        float h = movementInput.x;
+        r += Mathf.Round((h * 100f) / 100f);
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+    public float JoystickV()
+    {
+
+        float r = 0.0f;
+        float v = movementInput.y;
+        r += Mathf.Round((v * 100f) / 100f);
+        //r -= 1;
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+
+    public Vector3 MainJoystick()
+    {
+        return new Vector3(JoystickH(), 0, JoystickV());
+    }
+
+    void RotateMovement(Vector3 movement)
+    {
+        _angle = Mathf.Atan2(movement.x, movement.z);
+        _angle = Mathf.Rad2Deg * _angle;
+
+        targetRotation = Quaternion.Euler(0, _angle, 0);
+        transform.rotation = targetRotation;
+
+        this.GetComponent<Rigidbody>().MovePosition(transform.position + (transform.forward * movementSpeed * Time.deltaTime));
+    }
+
+    void Movement()
+    {
+        Vector3 movement = MainJoystick();
+        RotateMovement(movement);
+    }
+
+
+    //when player moves to next side, outside script calls this
+    void ChangePlayerFaceStatus(faceStatus newFaceStatus, Transform newPos)
+    {
+        playerFaceStatus = newFaceStatus;
+        //rotation is just as important as position, whatever direction player is facing that is their forward
+        transform.position = newPos.position;
+        transform.rotation = newPos.rotation;
+    }
+}
