f753bd133b8a7a302f0d386d14723ed6e34ad79f
WeaselB64 <morrisonwesley@hotmail.com>
2020-09-08 15:52:20 -0700

    Moved Files
    Added teleporter scripts, moved scripts I worked on to folder

---
+++ Assets/Scripts/Wesley's Scripts/PlayerTele.cs
@@ -0,0 +1,35 @@
+﻿//Wesley Morrison
+//9/3/2020
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerTele : MonoBehaviour
+{
+    public GameObject teleporterTracker;//Assign before load, set to private if unneeded
+
+    private void Start()
+    {
+        teleporterTracker = GameObject.FindGameObjectWithTag("GoalCheck"); //assumes we check on construction of the player, with a new player every level
+    }
+
+
+
+    private void OnTriggerEnter(Collider other) //Goals need to be triggers, have a trigger tag
+    {
+        if(other.tag == "Goal")
+        {
+            other.GetComponent<TeleBool>().active = true;
+            if (teleporterTracker.GetComponent<TeleporterScript>().GoalCheck(teleporterTracker.GetComponent<TeleporterScript>().teleporters))
+            {
+                int currentScene = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentScene + 1);
+            }
+        }
+    }
+
+
+
+}
