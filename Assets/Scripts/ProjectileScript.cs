5a48cdaec6c23d49e22c07d29f7474413b5d2f39
WeaselB64 <morrisonwesley@hotmail.com>
2020-09-08 13:10:37 -0700

    Shoot
    Working shoot scripts, copy-paste into relavent scripts

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,30 @@
+﻿//Wesley Morrison
+//9/7/2020
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public float speed; //speed modifiable by level designer
+    public int damage; //How much damage the projectile does, modifiable by power ups
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move_Forward();
+    }
+
+
+    void Move_Forward() //Moves projectile along axis (may later be modified to towards Target)
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+}
