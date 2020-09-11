0ba09f0349edbb2770416d5dc3549e494781a646
WeaselB64 <morrisonwesley@hotmail.com>
2020-09-11 14:14:13 -0700

    BigUpdate
    Player Logic script with accessable ints
    10% chance to crit added
    PlayerData prefab added

--- Assets/Scripts/Wesley's Scripts/ProjectileScript.cs
+++ Assets/Scripts/Wesley's Scripts/ProjectileScript.cs
@@ -8,12 +8,14 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     public float speed; //speed modifiable by level designer
-    public int damage; //How much damage the projectile does, modifiable by power ups
+    private int damage; //How much damage the projectile does, modifiable by power ups, get from player logic
+    private GameObject Controller; //Reference to GameController
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Controller = GameObject.FindGameObjectWithTag("GameController");
+        damage = Controller.GetComponent<PlayerLogic>().damage;
     }
 
     // Update is called once per frame
@@ -27,5 +29,17 @@ public class ProjectileScript : MonoBehaviour
     void Move_Forward() //Moves projectile along axis (may later be modified to towards Target)
     {
         transform.Translate(this.transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy") //Assuming enemies will have this tag.
+        {
+            other.GetComponent<BaseEnemy>().health -= damage; //This is normal damage
+            if(Random.Range(0f,10f)< 1) //This is a crit
+            {
+                other.GetComponent<BaseEnemy>().health -= damage;
+            }
+        }
     }
 }
