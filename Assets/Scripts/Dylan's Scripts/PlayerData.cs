e522fa52b5d8b420afe06b0743c5c3d8441b3cfe
Dylan Loe <dylanloe@gmail.com>
2020-09-14 15:32:49 -0700

    Start of PlayerData scriptable obj
    Keeps track of levels and score for progression

---
+++ Assets/Scripts/Dylan's Scripts/PlayerData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScritableObjects/PlayerData", order = 1)]
+public class PlayerData : ScriptableObject
+{
+    [Header("Current level player is on: ZERO INDEXED")]
+    public int OnLevel = 0;
+    //0 = level 1 and so on
+
+    [Header("Player score")]
+    public int score = 0;
+
+    [Header("Player Currency")]
+    public int currency;
+
+    [Header("Player Currency")]
+    public Scene[] levelNames;
+
+
+
+    //called when level beat
+    public void BeatLevel()
+    {
+        OnLevel++;
+
+    }
+
+    public void LoadNextLevel()
+    {
+        Debug.Log("Loading Next Level: " + levelNames[OnLevel].name);
+        SceneManager.LoadScene(levelNames[OnLevel].name);
+    }
+}
